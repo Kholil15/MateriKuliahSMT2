50c855a016023a9598d56e4e1885e559488ae6a6
Kholil15 <kholillummah@gmail.com>
2026-04-29 10:42:59 +0700

    kholil

---
+++ Pertemuan-4/SetHimpunan.py
@@ -0,0 +1,11 @@
+buah = {"apel", "jeruk", "mangga"}
+
+print("Buah:", buah)
+
+# menambah buah
+buah.add("pisang")
+print("buat setelah ditambah:", buah)
+
+# menghapus buah
+buah.remove("jeruk")
+print("Setelah dikurangi:", buah)
